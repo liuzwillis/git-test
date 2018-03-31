0bc5dfa8b2d05b684af75483e1ebc7ede3140844
willis <liuzw3@163.com>
2018-03-31 11:13:25 +0800

    a change of py

--- one/test.py
+++ one/test.py
@@ -1,4 +1,20 @@
-for i in range(33):
-    if i%3 == 0:
-        print(i)
+def get_num():
+    while True:
+        num = input("input a number between [0, 100), quit with \'quit\' or \'exit\':  ")
+        if num in ('quit','exit'):
+            break
+        try:
+            num = int(num)
+        except ValueError:
+            print('not a number')
+        else:
+            if num in range(100):
+                print('number: {}'.format(num))
+            else:
+                print('number {} out range'.format(num))
+    return num
+
+
+if __name__ == '__main__':
+    get_num()
 
